189e43c8e29b156f0a81296fa4ae38fdc0a2290c
hammami salem <salem@MacBook-Pro-de-hammami.local>
2023-10-28 01:41:46 +0100

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import Starred from './Starred';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="starred" element={<Starred />} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
     </div>
   );
 }
